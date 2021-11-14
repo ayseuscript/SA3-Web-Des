2d584d963596536e174cdf1b4457c2a6a8736662
ayseuscript <94310230+ayseuscript@users.noreply.github.com>
2021-11-15 00:50:21 +0800

    Add files via upload

---
+++ Product_Site_Script.js
@@ -0,0 +1,16 @@
+
+    function togglePopup_start(){
+  document.getElementById("popup-start").classList.toggle("active");
+        }
+        function togglePopup_1(){
+  document.getElementById("popup-1").classList.toggle("active");
+        }
+            function togglePopup_2(){
+  document.getElementById("popup-2").classList.toggle("active");
+            }
+    function togglePopup_3(){
+  document.getElementById("popup-3").classList.toggle("active");
+            }
+function togglePopup_custom(){
+  document.getElementById("popup-custom").classList.toggle("active");
+            }
